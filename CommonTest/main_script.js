c9721a7e33b18be6d42ecc4cf7d7b2f9149d8a08
atoy322 <atoy3022@gmail.com>
2022-01-26 14:31:41 +0900

    update

---
+++ CommonTest/main_script.js
@@ -0,0 +1,20 @@
+var label = "共通テスト1日目";
+var target_date = new Date(2023, 1-1, 14, 0, 0, 0, 0);
+
+
+function onload() {
+    document.getElementById("label").innerHTML = label + "まであと";
+    document.getElementById("unit").innerHTML = "秒";
+    setInterval(main, 10);
+}
+
+function main() {
+    var headline = document.getElementById("time");
+    var now = new Date();
+
+    var dt = target_date.getTime() - now.getTime();
+    dt /= 1000;
+    dt = dt.toFixed(2);
+    headline.innerHTML = dt;
+}
+
